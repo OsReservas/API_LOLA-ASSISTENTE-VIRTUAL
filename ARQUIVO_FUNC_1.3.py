6f0bbde9527e92e5961f5e31c9f9cdf91460c1f7
stopalien <marialice_tjs@hotmail.com>
2022-03-30 18:52:48 -0300

    versao 2

--- ARQUIVO_FUNC_1.3.py
+++ ARQUIVO_FUNC_1.3.py
@@ -1,6 +1,5 @@
 import speech_recognition as sr
 import pyttsx3
-import datetime
 import wikipedia
 import datetime
 import playsound
@@ -8,54 +7,36 @@ import playsound
 audio = sr.Recognizer()
 maquina = pyttsx3.init()
 
-def executa_comando():
+def executa_comando(pular_dora):
     with sr.Microphone() as source:
-        audio.adjust_for_ambient_noise(source)
+        #audio.adjust_for_ambient_noise(source)
         print("Ouvindo")
         voz = audio.listen(source)
     try:
         comando = audio.recognize_google(voz, language="pt-BR")
         comando = comando.lower()
 
-        if "dora" in comando:
+        if "dora" in comando or pular_dora == True:
             comando = comando.replace("dora", "")
-            maquina.runAndWait()
             if 'bom' and 'dia' in comando.lower():
                 maquina.say('bom dia! o que vamos fazer?')
-                maquina.runAndWait()
             elif 'boa' and 'tarde' in comando.lower():
                 maquina.say('boa tarde! o que vamos fazer?')
-                maquina.runAndWait()
             elif 'boa' and 'noite' in comando.lower():
                 maquina.say('boa noite! o que vamos fazer?')
-                maquina.runAndWait()
-            print("Você disse: " + comando)
-    except:
-        print("Não escutei")
-executa_comando()
-def executa_comando():
-    with sr.Microphone() as source:
-        audio.adjust_for_ambient_noise(source)
-        print("Ouvindo")
-        voz = audio.listen(source)
-        try:
-            comando = audio.recognize_google(voz, language="pt-BR")
-            comando = comando.lower()
-            if "horas" in comando:
-                hora = datetime.datetime.now().strftime("%H horas e %M minutos")
-                maquina.say("Agora são" + hora)
-                maquina.runAndWait()
+            elif "horas" in comando:
+                hora = datetime.datetime.now().strftime("%H:%M")
+                print(hora)
+                maquina.say("Agora são " + hora)
             elif "procure " in comando:
                 procurar = comando.replace("procure", " ")
                 wikipedia.set_lang("pt")
                 resultado = wikipedia.summary(procurar, 2)
                 maquina.say(resultado)
-                maquina.runAndWait()
             elif "soletre" in comando:
                 comando = comando.replace("soletre", " ")
                 for letra in comando:
                     maquina.say(letra)
-                    maquina.runAndWait()
             elif "patinho" in comando:
                 comando = comando.replace("historia do", "")
                 playsound.playsound('patinho.mp3')
@@ -65,7 +46,16 @@ def executa_comando():
             elif "porquinho" in comando:
                 comando = comando.replace("historia do", "")
                 playsound.playsound('porquinho.mp3')
-        except:
-            print("Não escutei")
-        return comando
-executa_comando()
+            elif "sair" in comando:
+                print("Tchau amiguinho")
+                return
+            maquina.runAndWait()
+            print("Você disse: " + comando)
+            executa_comando(True)
+        else:
+            print("não detectou dora")
+    except:
+        print("Não escutei")
+        executa_comando(pular_dora)
+
+executa_comando(False)
