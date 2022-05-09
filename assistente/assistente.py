b28dbceb5929f364ad4937c38fa8de3ab5324c54
Marcelo Silva <101959064+marcelosilva7@users.noreply.github.com>
2022-05-08 22:50:39 -0300

    Primeira versão da assistente divido em defs

---
+++ assistente/assistente.py
@@ -0,0 +1,16 @@
+import microfone
+import jogo
+import historia
+import pyttsx3
+maquina = pyttsx3.init()
+voices = maquina.getProperty('voices')
+maquina.setProperty('voice', voices[-2].id)
+
+maquina.say('Ola, oque vamos fazer hoje? ')
+maquina.runAndWait()
+mic = microfone.abrir_mic()
+
+if 'jogo' in mic:
+    jogo.jogo()
+if 'historia' in mic:
+    historia.historia()
