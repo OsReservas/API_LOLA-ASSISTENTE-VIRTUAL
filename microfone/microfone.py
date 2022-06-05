49d5c8c7faf0ed8a9e9510e5fad3d1957d6a1912
ThiagoChaves13 <102191484+ThiagoChaves13@users.noreply.github.com>
2022-06-05 11:24:39 -0300

    Add files via upload

--- microfone/microfone.py
+++ microfone/microfone.py
@@ -1,17 +1,26 @@
+from sys import flags
+from turtle import back
+
+
 def abrir_mic():
     import speech_recognition as sr
     import unidecode
+    from voz import maquina_voz
+    comando = None
+    flags = True
+    while flags:
+        audio = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Ouvindo")
+            voz = audio.listen(source)
+        try:
+            comando = audio.recognize_google(voz, language="pt-BR")
+            comando = comando.lower()
+            comando = unidecode.unidecode(comando)
+            flags = False
 
-    audio = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Ouvindo")
-        voz = audio.listen(source)
-    try:
-        comando = audio.recognize_google(voz, language="pt-BR")
-        comando = comando.lower()
-        comando = unidecode.unidecode(comando)
-        print(comando)
-    except:
-        print("Não escutei")
+        except:
+            print("Não escutei")
+            maquina_voz('Desculpe não entendi, poderia repetir')
+    print(comando)
     return comando
-
