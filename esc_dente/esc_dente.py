4db8432118eed78c96e218f6ae80d9dc2e59fd95
ThiagoChaves13 <102191484+ThiagoChaves13@users.noreply.github.com>
2022-06-05 11:26:10 -0300

    Add files via upload

---
+++ esc_dente/esc_dente.py
@@ -0,0 +1,53 @@
+def escovar_dentes ():
+    from voz import maquina_voz
+    from time import sleep
+    maquina_voz("ESTA NA HORA DE ESCOVARMOS OS DENTES !" )
+    sleep(1)
+    maquina_voz("VOU TE ENSINAR O PASSO A PASSO PARA APRENDEMOS UMA BOA ESCOVAÇÃO, LIVRE DE CARES")
+    sleep(1)
+    maquina_voz("O PRIMEIRO PASSO É COLOCAR UM POUCO DE PASTA DE DENTES NA ESCOVA")
+    sleep(1)
+    maquina_voz('VAMOS LA, PEGUE A PASTA DE DENTE E APERTE SUAVEMENTE SOBRE A ESCOVA, ATÉ SAIR UM PINGUINHO DE PASTA')
+    sleep(1)
+    maquina_voz("SEGUNDO PASSO, COMEÇE LIMPANDO OS DENTES DO FUNDO DA BOCA, POR 5 SEUNGOS")
+    sleep(1)
+    maquina_voz('VAMOS LA, COMECE QUANDO COMEÇAR A CONTAGEM..')
+    sleep(1)
+    maquina_voz('1')
+    sleep(1)
+    maquina_voz('2')
+    sleep(1)
+    maquina_voz('3')
+    sleep(1)
+    maquina_voz('4')
+    sleep(1)
+    maquina_voz('5')
+    sleep(1) 
+    maquina_voz("LIMPANDO OS DENTES DE CIMA E DE BAIXO  PELA PARTE DE FORA, A ESCOVA TEM QUE ESTAR UM POUCO INCLINADA E OS MOVIMENTOS TEM QUE SER DE CIMA PARA BAIXO SUAVES E CURTOS,PODEMOS FAZER TAMBÉM PEQUENOS CIRCULOS.")
+    sleep(5)
+
+
+    maquina_voz("TERCEIRO PASSO_ LIMPEM A PARTE DE DENTRO DOS DENTES DE CIMA PRA BAIXO,LIMPANDO BEM OS DENTES DO FUNDO.")
+    sleep(5)
+    
+    maquina_voz("QUARTO PASSO_ESCOVEM A BORDA DOS DENTES COM MOVIMENTOS PARA FRENTE E PARA TRÁS")
+    sleep(5)
+
+    maquina_voz("QUINTO PASSO_VAMOS AGORA LIMPAR O RESTO DA BOCA! PASSE A ESCOVA SUAVEMENTE PELO INTERIOR DE SUAS  BUCHECHAS E PELA LINGUA.")
+    sleep(5)
+    maquina_voz("SEXTO PASSO_VAMOS CUSPIR A ESPUMA QUE SE ACUMULOU E ENCHER A BOCA COM ÁGUA FAZENDO UMA LAVAGEM GERAL E ENXAGUE,DEPOIS DESCARTANDO A MESMA.")
+    sleep(1)
+
+    maquina_voz("AÍ VÃO ALGUMAS DICAS RÁPIDAS:")
+    sleep(1)
+
+    maquina_voz("_UTILIZAR SEMPRE QUE POSSÍVEL O FIO DENTAL")
+    sleep(1)
+
+    maquina_voz("_UTILIZAR CREME COM UMA TAXA DE FLUOR")
+    sleep(1)
+
+    maquina_voz("_UMA BOA ALIMENTAÇÃO RICA EM PROTEINAS E SAUDÁVEIS")
+    sleep(1)
+
+    maquina_voz("_SEMPRE QUE POSSÍVEL SUBSTITUIR A ESCOVA QUANDO ESTIVER DESGASTADA")
